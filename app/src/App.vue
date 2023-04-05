6e06f608897dd3bd0711324b31eaf91bbfcbaab0
oczywisty <pawel.jasiolek@gmail.com>
2023-04-05 15:23:58 +0200

    added vue js

---
+++ app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MainView />
+  </div>
+</template>
+
+<script>
+import MainView from './views/MainView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainView
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ app/src/views/MainView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Pieczenie</h1>
+      <button id="fetchButton">Pobierz plan pieczenia</button>
+      <ul id="rowsList" class="database-list"></ul>
+
+      <table id="data-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Magazyn (before)</th>
+            <th>Magazyn (after)</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+      <br /><br />
+      <button id="updateButton">Zapisz stany magazynowe</button>
+      <p id="progressCounter"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainView',
+  data() {
+    return {
+      // Add data properties here
+    }
+  },
+  methods: {
+    // Add methods here
+  },
+  mounted() {
+    // Add lifecycle hook logic here
+  }
+}
+</script>
